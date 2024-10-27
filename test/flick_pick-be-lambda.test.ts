ed43aa01e0959ee920a61c539ecd6c91b062874d
Dinuka Dilshan <jadinukadilshan@gmail.com>
2024-10-27 11:54:15 +0530

    Initial commit

---
+++ test/flick_pick-be-lambda.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FlickPickBeLambda from '../lib/flick_pick-be-lambda-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/flick_pick-be-lambda-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FlickPickBeLambda.FlickPickBeLambdaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
